18433ad1d9f3cee68f04fe0d4b3ab5b171fefc8f
Kriangkrai Prommaithong <kr.prommaithong@gmail.com>
2022-12-23 11:27:04 +0700

    Logback Masking

--- src/main/java/com/krungsri/aycap/payment/controller/OnlinePaymentController.java
+++ src/main/java/com/krungsri/aycap/payment/controller/OnlinePaymentController.java
@@ -3,12 +3,16 @@ package com.krungsri.aycap.payment.controller;
 import com.krungsri.aycap.payment.models.request.RequestModel;
 import com.krungsri.aycap.payment.models.response.ResponseModel;
 import com.krungsri.aycap.payment.services.OnlinePaymentService;
+import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @CrossOrigin
 @RestController
@@ -24,7 +28,22 @@ public class OnlinePaymentController extends BaseController {
 
   @GetMapping(value = "/test")
   public ResponseEntity<ResponseModel> onlinePayment(@RequestBody RequestModel obj) {
-    log.info("[REQUEST] >> {}", obj);
+
+    Map<String, String> user = new HashMap<>();
+    user.put("user_id", "87656");
+    user.put("SSN", "786445563");
+    user.put("address", "22 Street");
+    user.put("city", "Chicago");
+    user.put("Country", "U.S.");
+    user.put("ip_address", "192.168.1.1");
+    user.put("cardNo", "1234567894561234");
+    user.put("firstName", "Kriangkrai");
+    user.put("lastName", "Prommaithong");
+    user.put("email_id", "spring@baeldung.com");
+    JSONObject userDetails = new JSONObject(user);
+
+    log.info("User JSON: {}", userDetails);
+//    log.info("[REQUEST] >> {}", obj);
     try {
       onlinePaymentService.onSavePayment();
       responseModel = onlinePaymentService.onGetPayment();
